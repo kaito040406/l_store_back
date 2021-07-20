f930094c459c340709ff44e48c3ad380fba251b1
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-07-20 12:17:38 +0900

    修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000" # React側はポート番号3000で作るので「localhost:3000」を指定
+    origins ENV['FRONT_URL'] # React側はポート番号3000で作るので「localhost:3000」を指定
 
     resource "*",
       headers: :any,
