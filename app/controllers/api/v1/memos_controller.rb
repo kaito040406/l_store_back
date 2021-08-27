6467f947b67cc27bbc878b093c9a7858a1cd900a
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-08-27 19:04:20 +0900

    20210827

--- app/controllers/api/v1/memos_controller.rb
+++ app/controllers/api/v1/memos_controller.rb
@@ -2,17 +2,33 @@ class Api::V1::MemosController < ApplicationController
   before_action :authenticate_api_v1_user!
   def index
     trg_user = LineCustomer.find(params[:line_customer_id])
-    memos = Memo.where(line_customer_id: trg_user.line_customer_id)
+    memos = LineCustomerMemo.where(line_customer_id: trg_user.id)
     render json: memos
   end
 
   def create
-    memos = Memo.create(memo_params)
+    memo = LineCustomerMemo.create(line_customer_id: params[:line_customer_id], body: params[:body])
+
+    render json: memo
   end
 
-  private
-  def memo_params
-    params.require(:memos).permit(:line_customer_id, :body)
+  def update
+    # 対象のデータ取得
+    trg_memo = LineCustomerMemo.find(params[:id])
+
+    # 対象を更新
+    memo = trg_memo.update(body: params[:body])
+
+    render json: memo
   end
 
+  def delete
+    # 対象のデータ取得
+    trg_memo = LineCustomerMemo.find(params[:id])
+
+    # 対象を削除
+    memo = trg_memo.destroy
+
+    render json: memo
+  end
 end
