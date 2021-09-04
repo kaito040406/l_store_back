10ab40c83951607aea3b28dfdd66b5d7657096a1
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-09-04 09:46:14 +0900

    edit

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,5 +1,5 @@
 class WebhookController < ApplicationController
-  skip_before_action :verify_authenticity_token,except: :stripe_webhook
+  # skip_before_action :verify_authenticity_token,except: :stripe_webhook
 
   def stripe_webhook
 
